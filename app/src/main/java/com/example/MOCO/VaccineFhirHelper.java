94a09d1c6e0a6209aceb505b4ef78e3e9831cd38
Daniel Schermann <e11777772@student.tuwien.ac.at>
2022-06-20 18:49:20 +0200

    fixed the margin of search button of vaccine page

--- app/src/main/java/com/example/MOCO/VaccineFhirHelper.java
+++ app/src/main/java/com/example/MOCO/VaccineFhirHelper.java
@@ -143,6 +143,7 @@ public class VaccineFhirHelper {
         return BundleUtil.toListOfResourcesOfType(ctx, bundle, ImmunizationRecommendation.class);
     }
 
+
     public List<ImmunizationRecommendation> getRecommendationsTest(){
         Bundle bundle = client.search().forResource(ImmunizationRecommendation.class)
                 .where(new TokenClientParam("_id").exactly().code("a01d428f-2da5-462d-806b-6b7175068901"))
