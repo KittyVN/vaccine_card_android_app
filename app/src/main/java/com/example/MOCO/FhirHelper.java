ddb05f764107dcbb8e7dd218195a8935b1d5708c
Ruth Slavicky <e0271551@student.tuwien.ac.at>
2022-06-21 15:35:35 +0200

    Add TiterSearch and refactor FhirHelper.java

---
+++ app/src/main/java/com/example/MOCO/FhirHelper.java
@@ -0,0 +1,133 @@
+package com.example.MOCO;
+
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Enumerations;
+import org.hl7.fhir.r4.model.Immunization;
+import org.hl7.fhir.r4.model.ImmunizationRecommendation;
+import org.hl7.fhir.r4.model.Location;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Organization;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.SearchParameter;
+
+import java.util.List;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
+import ca.uhn.fhir.util.BundleUtil;
+
+public class FhirHelper {
+
+    private IGenericClient client;
+    public FhirContext ctx;
+
+    private String url2 = "https://hapi.fhir.org/baseR4";
+    private String url3 = "http://localhost:8080/fhir";
+    private String url4 = "https://spark.incendi.no/fhir";
+    private String url5 = "https://sqlonfhir-r4.azurewebsites.net/fhir";
+    private String url1 = "https://server.fire.ly/r4";
+    private String ourServerUrl = "http://lukasletuhalmg.synology.me:12345/fhir";
+
+
+    public FhirHelper() {
+        ctx = FhirContext.forR4();
+        client = ctx.newRestfulGenericClient(ourServerUrl);
+    }
+
+    public void uploadSearchParameter() {
+        // Create a search parameter definition
+        SearchParameter countrySP = new SearchParameter();
+        countrySP.addBase("ImmunizationRecommendation");
+        countrySP.setCode("country");
+        countrySP.setType(Enumerations.SearchParamType.TOKEN);
+        countrySP.setTitle("Country");
+        countrySP.setExpression("ImmunizationRecommendation.extension('http://tuwien.ac.at/moco/fhir/StructureDefinition/PlusCountryName')");
+        countrySP.setXpathUsage(SearchParameter.XPathUsageType.NORMAL);
+        countrySP.setStatus(Enumerations.PublicationStatus.ACTIVE);
+        client.create().resource(countrySP).execute();
+    }
+
+
+    public List<Immunization> getVaccines(String vaccine) {
+        // Invoke the client
+
+        String searchUrl = "Immunization?vaccine-code:text=" + vaccine;
+
+        Bundle bundle = client.search()
+                .byUrl(searchUrl)
+                .returnBundle(Bundle.class)
+                .execute();
+        return BundleUtil.toListOfResourcesOfType(ctx, bundle, Immunization.class);
+    }
+
+    public List<Immunization> getAllVaccines() {
+        // Invoke the client
+        Bundle bundle = client.search().forResource(Immunization.class)
+                .prettyPrint()
+                .returnBundle(Bundle.class)
+                .execute();
+        return BundleUtil.toListOfResourcesOfType(ctx, bundle, Immunization.class);
+    }
+
+    public List<Observation> getAllTiters() {
+        // Invoke the client
+        String searchUrl = "Observation?_sort=-date";
+
+        Bundle bundle = client.search()
+                .byUrl(searchUrl)
+                .returnBundle(Bundle.class)
+                .execute();
+        return BundleUtil.toListOfResourcesOfType(ctx, bundle, Observation.class);
+    }
+
+    public List<Observation> getTiter(String titer) {
+        // Invoke the client
+
+        String searchUrl = "Observation?_sort=-date&code:text=" + titer;
+
+        Bundle bundle = client.search()
+                .byUrl(searchUrl)
+                .returnBundle(Bundle.class)
+                .execute();
+        return BundleUtil.toListOfResourcesOfType(ctx, bundle, Observation.class);
+    }
+
+    public List<ImmunizationRecommendation> getRecommendation(String country) {
+        Bundle bundle = client.search().forResource(ImmunizationRecommendation.class)
+                .where(new TokenClientParam("country").exactly().code(country))
+                .prettyPrint()
+                .returnBundle(Bundle.class)
+                .execute();
+        return BundleUtil.toListOfResourcesOfType(ctx, bundle, ImmunizationRecommendation.class);
+    }
+
+
+    public List<ImmunizationRecommendation> getRecommendationsTest() {
+        Bundle bundle = client.search().forResource(ImmunizationRecommendation.class)
+                .where(new TokenClientParam("_id").exactly().code("a01d428f-2da5-462d-806b-6b7175068901"))
+                .prettyPrint()
+                .returnBundle(Bundle.class)
+                .execute();
+        return BundleUtil.toListOfResourcesOfType(ctx, bundle, ImmunizationRecommendation.class);
+    }
+
+
+    public Organization getOrganization(String ref) {
+        return client.read().resource(Organization.class).withId(ref).execute();
+    }
+
+    public Location getLocation(String ref) {
+        return client.read().resource(Location.class).withId(ref).execute();
+    }
+
+
+    public Patient getExamplePatient() {
+        return client.read().resource(Patient.class).withId("example").execute();
+    }
+
+    public IGenericClient getClient() {
+        return client;
+    }
+
+}
--- app/src/main/java/com/example/MOCO/TiterActivity.java
+++ app/src/main/java/com/example/MOCO/TiterActivity.java
@@ -5,16 +5,20 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import org.hl7.fhir.r4.model.DateTimeType;
-import org.hl7.fhir.r4.model.Immunization;
 import org.hl7.fhir.r4.model.Observation;
-import org.hl7.fhir.r4.model.Type;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -27,9 +31,11 @@ public class TiterActivity extends AppCompatActivity {
     private List<String> titerDate = new ArrayList<String>();
     private List<String> titerValue = new ArrayList<String>();
 
-
+    private String enteredSearchTarget;
     private RecyclerView rvTiter;
     private Context ctx = this;
+    private TiterActivity activity = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,24 +69,63 @@ public class TiterActivity extends AppCompatActivity {
             }
         });
 
+        TextInputEditText tvSearch = (TextInputEditText) findViewById(R.id.textInputTiter);
+        ImageButton btnCountrySearch = (ImageButton) findViewById(R.id.btnSearch2);
         new TiterTask(this).execute();
+
+        tvSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    handled = true;
+                    enteredSearchTarget = tvSearch.getText().toString();
+                    tvSearch.setText("");
+                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    new TiterTask(activity).execute();
+
+                }
+                return handled;
+            }
+        });
+
+
+        btnCountrySearch.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enteredSearchTarget = tvSearch.getText().toString();
+                tvSearch.setText("");
+                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                new TiterTask(activity).execute();
+            }
+        });
+
 
     }
 
     private static class TiterTask extends AsyncTask<Void, Object, List<Observation>> {
 
         private WeakReference<TiterActivity> activityReference;
+        private TiterActivity activity;
 
         // only retain a weak reference to the activity
         TiterTask(TiterActivity context) {
             activityReference = new WeakReference<>(context);
+            activity = activityReference.get();
         }
         @Override
         protected List<Observation> doInBackground(Void... voids) {
-            VaccineFhirHelper gcm = new VaccineFhirHelper();
+            FhirHelper gcm = new FhirHelper();
 
-            //return list;
-            List<Observation> listTiters = gcm.getAllTiters();
+            List<Observation> listTiters = new ArrayList<>();
+
+            if (activity.enteredSearchTarget == "" || activity.enteredSearchTarget == null){
+                listTiters = gcm.getAllTiters();
+            }else {
+                listTiters = gcm.getTiter(activity.enteredSearchTarget);
+            }
 
             return listTiters;
         }
@@ -88,34 +133,41 @@ public class TiterActivity extends AppCompatActivity {
         protected void onPostExecute(List<Observation> titers) {
             TiterActivity activity = activityReference.get();
 
-            for (Observation titer : titers) {
-                activity.titerTyp.add(titer.getCode().getCoding().get(0).getDisplay());
-                if (titer.getEffective() != null && titer.getEffective().fhirType() == "dateTime"){
-                    activity.titerDate.add(titer.getEffectiveDateTimeType().asStringValue());
-                }else if (titer.getEffective() != null && titer.getEffective().fhirType() == "Period"){
-                    activity.titerDate.add(titer.getEffectivePeriod().getEnd().toString());
-                }else activity.titerDate.add("empty");
+            activity.titerDate.clear();
+            activity.titerLabor.clear();
+            activity.titerTyp.clear();
+            activity.titerValue.clear();
 
-                if (titer.getValue() != null && titer.getValue().fhirType() == "Quantity"){
-                    activity.titerValue.add(titer.getValueQuantity().getValue().toString() + " " + titer.getValueQuantity().getCode());
-                }else if (titer.getValue() != null){
-                    activity.titerValue.add(titer.getValue().fhirType());
-                }else {
-                    activity.titerValue.add("no value");
+            if (titers.size() != 0) {
+                for (Observation titer : titers) {
+                    activity.titerTyp.add(titer.getCode().getCoding().get(0).getDisplay());
+                    if (titer.getEffective() != null && titer.getEffective().fhirType() == "dateTime") {
+                        activity.titerDate.add(titer.getEffectiveDateTimeType().asStringValue());
+                    } else if (titer.getEffective() != null && titer.getEffective().fhirType() == "Period") {
+                        activity.titerDate.add(titer.getEffectivePeriod().getEnd().toString());
+                    } else activity.titerDate.add("empty");
+
+                    if (titer.getValue() != null && titer.getValue().fhirType() == "Quantity") {
+                        activity.titerValue.add(titer.getValueQuantity().getValue().toString() + " " + titer.getValueQuantity().getCode());
+                    } else if (titer.getValue() != null) {
+                        activity.titerValue.add(titer.getValue().fhirType());
+                    } else {
+                        activity.titerValue.add("no value");
+                    }
+
+                    if (titer.getPerformer().size() > 0) {
+                        activity.titerLabor.add(titer.getPerformer().get(0).getDisplay());
+                    } else {
+                        activity.titerLabor.add("Unknown");
+                    }
+
                 }
+                activity.rvTiter = activity.findViewById(R.id.rvTiter);
 
-                if (titer.getPerformer().size() > 0){
-                    activity.titerLabor.add(titer.getPerformer().get(0).getDisplay());
-                }else {
-                    activity.titerLabor.add("Unknown");
-                }
-
+                TiterAdapter titerAdapter = new TiterAdapter(activity.ctx, activity.titerTyp, activity.titerLabor, activity.titerDate, activity.titerValue);
+                activity.rvTiter.setAdapter(titerAdapter);
+                activity.rvTiter.setLayoutManager(new LinearLayoutManager(activity.ctx));
             }
-            activity.rvTiter = activity.findViewById(R.id.rvTiter);
-
-            TiterAdapter titerAdapter = new TiterAdapter(activity.ctx, activity.titerTyp, activity.titerLabor, activity.titerDate, activity.titerValue);
-            activity.rvTiter.setAdapter(titerAdapter);
-            activity.rvTiter.setLayoutManager(new LinearLayoutManager(activity.ctx));
         }
     }
 }
